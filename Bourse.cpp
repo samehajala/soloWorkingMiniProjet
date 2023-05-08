37dff414248933113c6bfa52a037e6ec138f74ed
samehajala <118663582+samehajala@users.noreply.github.com>
2023-05-08 19:22:21 +0100

    first commit

---
+++ Bourse.cpp
@@ -0,0 +1,5 @@
+#include"Bourse.h"
+// bourse set
+//bourse multi map cle date valeur vector de prix journalier
+// bourse multi set
+
---
+++ Simulation.cpp
@@ -0,0 +1,81 @@
+#include"Simulation.h"
+using namespace std ;
+map<string , long> Simulation::executer(Bourse &bourse ,Trader& trader , Date dateDebut , Date dateFin,double solde )
+{
+    map<string,long> maSimulation;
+    PorteFeuille porteFeuille(solde) ;
+    Date datePrecedente ;
+    Date dateCourante=dateDebut ;
+    int NombreDesTransactionTotale=0 ;
+    int NombreDesTransactionAcheter=0 ;
+    int NombreDesTransactionVendre=0 ;
+    int NombreDesTransactionRien=0 ;
+    int NombreDesTransactionEchouees=0 ;
+    int nombreJourFermee=0 ;
+    while(dateCourante<=dateFin)
+    {
+
+        cout<<dateCourante<<endl ;
+        vector<string> actionsdisponiblesaujourdhui ;
+        actionsdisponiblesaujourdhui=bourse.getActionsDisponibleParDate(bourse.getDateCourante()) ;
+        if(actionsdisponiblesaujourdhui.size()==0)
+        {       cout<<"bonjour pas action "<<endl;
+                nombreJourFermee++;
+             dateCourante.passToNextDay() ;
+             bourse.PasserALaJourneeSuivante() ;
+        }
+        else{
+        int nombreTransactionParJour=0 ;
+        Transaction transaction=trader.choisirTransaction(bourse,porteFeuille) ;
+        if(transaction.getTypeTransaction()==Rien)
+        {
+            //cout<<"Rien faire"<<endl  ;
+            NombreDesTransactionRien++ ;
+        }
+
+        while((nombreTransactionParJour<100) && (transaction.getTypeTransaction()!=Rien))
+        {
+            if(transaction.getTypeTransaction()==Acheter)
+            {
+
+                Titre titreAchete(transaction.getNomAction(),transaction.getQuantite()) ;
+                if(porteFeuille.getSolde()>=bourse.getPrixJournalierParDatePourUneAction(dateCourante,transaction.getNomAction())*titreAchete.getQuantite())
+                {
+                    //cout<<"Achater\t"<<transaction.getNomAction()<<"\t"<<transaction.getQuantite()<<endl ;
+                    porteFeuille.acheterTitre(titreAchete,bourse.getPrixJournalierParDatePourUneAction(dateCourante,transaction.getNomAction())*titreAchete.getQuantite()) ;
+                    NombreDesTransactionAcheter++ ;
+
+                }
+                else
+                {
+                    //cout<<"La Transaction  "<<"Achater\t"<<transaction.getQuantite()<<"\t"<<transaction.getNomAction()<<"\test impossible"<<endl ;
+                    NombreDesTransactionEchouees++ ;
+                }
+            }
+            else
+            {
+                //cout<<"Vendre\t"<<transaction.getNomAction()<<"\t"<<transaction.getQuantite()<<endl ;
+                Titre titreVendre(transaction.getNomAction(),transaction.getQuantite()) ;
+                porteFeuille.vendreTitre(titreVendre,bourse.getPrixJournalierParDatePourUneAction(dateCourante,transaction.getNomAction())*titreVendre.getQuantite()) ;
+                NombreDesTransactionVendre ++ ;
+
+
+
+            }
+            transaction=trader.choisirTransaction(bourse,porteFeuille) ;
+            nombreTransactionParJour++ ;
+            NombreDesTransactionTotale++ ;
+        }
+        dateCourante.passToNextDay() ;
+        bourse.PasserALaJourneeSuivante() ;
+
+    }//fin du else
+    }
+        maSimulation["nombre Des Transactions Totales"]=NombreDesTransactionTotale ;
+        maSimulation["NombreDesTransactionAcheter"]=NombreDesTransactionAcheter ;
+        maSimulation["NombreDesTransactionEchouees"]=NombreDesTransactionEchouees;
+        maSimulation["NombreDesTransactionVendre"]=NombreDesTransactionVendre ;
+        maSimulation["solde"]=porteFeuille.getSolde() ;
+        cout<<"bonjour je fais les statistiquer"<<endl;
+return maSimulation ;
+}
